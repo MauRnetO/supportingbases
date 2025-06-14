884c1569d6d2f1313aeb06831c473ce43b5176dc
MauRnetO <maur.neto1996@gmail.com>
2025-06-14 03:15:57 -0300

    Atualização Agendamentos Part2

--- supabaseClient.ts
+++ supabaseClient.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+const supabaseUrl = https://wmwkgtixjpjjlivtjvgx.supabase.co
+
+const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indtd2tndGl4anBqamxpdnRqdmd4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDk4MzU1NzUsImV4cCI6MjA2NTQxMTU3NX0.fT1387KB903YvkOxQQkHhOwasdY69UWGyU2QhKKZEAE
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
