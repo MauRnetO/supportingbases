c99481abd303cb8504bd42823910338a33002315
MauRnetO <maur.neto1996@gmail.com>
2025-06-15 21:59:45 -0300

    Atualização definitiva part 3

--- lib/useAuth.ts
+++ lib/useAuth.ts
@@ -1,21 +1,35 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { supabase } from "../supabaseClient";
 
 export default function useAuth() {
   const router = useRouter();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      if (!session) router.push("/auth/login");
+      if (!session) {
+        router.push("/login");
+      } else {
+        setUser(session.user);
+      }
+      setLoading(false);
     });
 
-    const { data: listener } = supabase.auth.onAuthStateChange((event, session) => {
-      if (!session) router.push("/auth/login");
+    const { data: listener } = supabase.auth.onAuthStateChange((_event, session) => {
+      if (!session) {
+        router.push("/login");
+        setUser(null);
+      } else {
+        setUser(session.user);
+      }
     });
 
     return () => {
       listener.subscription.unsubscribe();
     };
   }, [router]);
+
+  return { user, loading };
 }
