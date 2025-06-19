92101382e0f147fc1d9e53c262c5246458f2a5ab
MauRnetO <maur.neto1996@gmail.com>
2025-06-18 23:22:48 -0300

    Login Debug part3

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
           setLoading(false);
           return;
         }
-        sessao = { user: userData.user }; // cria sessão manualmente com user
+        sessao = { user: userData.user } as any; // cria sessão manualmente com user
       }
 
       const user = sessao.user;
